4259c9615e733a091884b222793590986d1186ca
Gaurav Pandey <ggauravsigra@gmail.com>
2017-12-14 01:00:32 +0530

    New UI for Soundcom app

--- app/src/main/java/org/scorelab/soundcom/MainActivity.java
+++ app/src/main/java/org/scorelab/soundcom/MainActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.content.Intent;
 import android.media.MediaPlayer;
 import android.media.MediaRecorder;
+import android.os.Handler;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -15,20 +16,31 @@ import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
-    private ImageButton startBtn;
+    private ImageView startBtn;
+    TextView textView4;
+    private int seconds=0;
+    private boolean startRun;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getSupportActionBar().hide();
+        if(savedInstanceState != null){
+            seconds = savedInstanceState.getInt("seconds");
+            startRun=savedInstanceState.getBoolean("startRun");
+        }
 
-        startBtn = (ImageButton)findViewById(R.id.button1);
+        startBtn = (ImageView)findViewById(R.id.button1);
         startBtn.setOnClickListener(new View.OnClickListener() {
 
             @Override
@@ -37,9 +49,39 @@ public class MainActivity extends AppCompatActivity {
                 Intent svc=new Intent(getBaseContext(), RecordingService.class);
                 startService(svc);
 
+                Timer();
+                startRun=true;
 
-                finish(); //because I want to close the UI after service started
+
+              //  finish(); //because I don't want to close the UI after service started
             }
         });
+    }
+    public void onSaveInstanceState(Bundle saveInstanceState){
+        saveInstanceState.putInt("seconds", seconds);
+        saveInstanceState.putBoolean("startRun", startRun);
+    }
+    private void Timer(){
+        final TextView timeView = (TextView)findViewById(R.id.textView4);
+        final Handler handler = new Handler();
+        handler.post(new Runnable() {
+            @Override
+            public void run() {
+                int hours = seconds/3600;
+                int minutes = (seconds%3600)/60;
+                int secs = seconds%60;
+
+                String time = String.format("%d:%02d:%02d", hours, minutes, secs);
+
+                timeView.setText(time);
+
+                if(startRun){
+                    seconds++;
+                }
+
+                handler.postDelayed(this, 100);
+            }
+        });
+
     }
 }
